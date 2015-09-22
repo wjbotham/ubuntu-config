a4654ad869d33d079777663a2f365749d73b274c
Wesley J. Botham <wbotham@appliedsystems.com>
2015-09-22 10:21:37 -0500

    Add script that makes Windows share load on startup

---
+++ set_up_windows_share.sh
@@ -0,0 +1,6 @@
+# This script assumes you have an entry titled VirtualBox_Share in the Machine
+# Folders section of the Shared Folders tab for this VM.
+
+sudo mkdir /media/windows-share
+sudo mount -t vboxsf VirtualBox_Share /media/windows-share
+ln -s /media/windows-share/ ~/Desktop/WindowsShare
