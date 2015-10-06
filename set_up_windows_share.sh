ac98de7cd9601c9e2ccb234531120122c73c16fb
Wesley J. Botham <wbotham@appliedsystems.com>
2015-10-06 13:21:36 -0500

    Add some comments and remove sudo's

--- set_up_windows_share.sh
+++ set_up_windows_share.sh
@@ -1,7 +1,12 @@
 # This script assumes you have an entry titled VirtualBox_Share in the Machine
 # Folders section of the Shared Folders tab for this VM.
 
-sudo mkdir /media/windows-share
-sudo mount -t vboxsf VirtualBox_Share /media/windows-share
-ln -s /media/windows-share/ ~/Desktop/WindowsShare
-ln -s /media/windows-share/ ~/WindowsShare
+# Add this line to /etc/crontab: 
+#@reboot root /home/wesley/ubuntu-config/set_up_windows_share.sh /home/wesley/Projects
+# I guess. It doesn't work right now?
+
+# It takes one argument: The path where you want the share to be linked.
+
+mkdir /media/windows-share
+mount -t vboxsf VirtualBox_Share /media/windows-share
+ln -s /media/windows-share/ $1
