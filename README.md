8838314d917f07f1c7290eba4da4478c3304f584
Wesley J. Botham <wbotham@appliedsystems.com>
2017-12-04 11:05:56 -0600

    Finally realized it should be in /etc/rc.local lol

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ubuntu-config
 
-Britton suggested using the cron to set this up. Right now like whatevs though
+Add this line to /etc/rc.local right before the `exit 0`:
+
+sh /home/wesley/ubuntu-config/set_up_windows_share.sh /home/wesley/Projects
--- set_up_windows_share.sh
+++ set_up_windows_share.sh
@@ -1,9 +1,9 @@
 # This script assumes you have an entry titled VirtualBox_Share in the Machine
 # Folders section of the Shared Folders tab for this VM.
 
-# Add this line to /etc/crontab: 
-#@reboot root /home/wesley/ubuntu-config/set_up_windows_share.sh /home/wesley/Projects
-# I guess. It doesn't work right now?
+# Add this line to /etc/rc.local right before the `exit 0`:
+#
+# sh /home/wesley/ubuntu-config/set_up_windows_share.sh /home/wesley/Projects
 
 # It takes one argument: The path where you want the share to be linked.
 
